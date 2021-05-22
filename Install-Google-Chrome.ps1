3bd96e866f99c4256c2241344dcc7f311fafc14c
oleitao <olivleitao@gmail.com>
2021-05-22 23:10:40 +0100

    poweshell script to install google chrome browser

---
+++ Install-Google-Chrome.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$TempPath = $env:TEMP;
+	$SetupFile = "chrome_installer.exe"
+	Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $TempPath\$SetupFile
+	Start-Process -FilePath $TempPath\$SetupFile -Args "/silent /install" -Verb RunAs -Wait
+	Remove-Item $TempPath\$SetupFile
+	write-host -foregroundColor green "✔️  Google Chrome installed"
+	exit 0
+} catch {
+	write-error "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
